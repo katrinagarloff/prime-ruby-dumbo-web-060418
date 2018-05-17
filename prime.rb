f66723db0b1b423e46b2091d09ed634be4e898e1
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-17 19:19:55 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime? (is_prime) 
+  true_false = true
+  ar = []
+  
+  for i in 2..is_prime-1
+  ar.push(i)
+end
+
+
+ar.each do |i|
+  if is_prime % i == 0 
+    true_false = false
+end
+end
+return true_false
+end
+
